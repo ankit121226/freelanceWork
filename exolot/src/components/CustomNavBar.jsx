bf05afa00f16274a5eafcd53ff2e28ed3599fa9f
ankit121226 <kumarroushan197.com>
2019-11-20 22:44:09 +0530

    navbar dropdown menu chngs

---
+++ exolot/src/components/CustomNavBar.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const DefaultStyle = styled.div`
+  .navbar-light .navbar-nav .nav-item .nav-link{
+    color: #71c228;
+  }
+  .dropdown-item{
+   font-size: 14px;
+  }
+  @media (max-width : 760px){
+   
+  }
+`;
+export default DefaultStyle;
--- exolot/src/components/NavBar.jsx
+++ exolot/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-scroll";
 import { Navbar, Nav, Container,NavDropdown} from "react-bootstrap";
+import  DefaultStyle from './CustomNavBar'
 
 import SideNav from  "../components/SideNav";
 
@@ -32,13 +33,14 @@ class NavBar extends Component {
     }
     render() {
         return (
+         <DefaultStyle>
             <React.Fragment>
                 <Navbar
                     sticky="top"
                     id="navbar"
                     bg="light"
                     expand="lg"
-                    className="navbar navbar-expand-lg navbar-light bg-light"
+                    className="navbar navbar-expand-lg navbar-light bg-light custom-nav"
                     collapseOnSelect={true}
                 >
                 <Container>
@@ -77,52 +79,115 @@ class NavBar extends Component {
 
                             <Nav.Item>
                                 <NavDropdown title="Course Details" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#">ENGINEERING</NavDropdown.Item>
+                                    <NavDropdown title="MEDICAL" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">MBBS</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">BDS</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">MDS</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="AYURVEDA" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">BAMS</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">BHMS</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">BNYS</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="PHARMACY" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">B.PHARMA</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">D.PHARMA</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">PHARMD</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="PHYSIOTHRAPY" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">BPT</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">GNM</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">BSC.NURSING</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="ENGINEERING" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">MECHANICAL</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">CS</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">CIVIL</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">ELECTRICAL</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">ECE</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <Nav.Item style={{color: '#71c228'}}>
+                                    MBA
+                                  </Nav.Item>
+                                </NavDropdown>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <NavDropdown title="Courses" id="basic-nav-dropdown">
+                                    <NavDropdown title="MEDICAL" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">MBBS</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">BDS</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">MDS</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="AYURVEDA" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">BAMS</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">BHMS</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">BNYS</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="PHARMACY" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">B.PHARMA</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">D.PHARMA</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">PHARMD</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="PHYSIOTHRAPY" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">BPT</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">GNM</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">BSC.NURSING</NavDropdown.Item> 
+                                    </NavDropdown>
+                                    <NavDropdown title="ENGINEERING" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#">MECHANICAL</NavDropdown.Item>
+                                        <NavDropdown.Item href="#">CS</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">CIVIL</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">ELECTRICAL</NavDropdown.Item> 
+                                        <NavDropdown.Item href="#">ECE</NavDropdown.Item> 
+                                    </NavDropdown>
+                                </NavDropdown>
+                            </Nav.Item>
+                            {/* <Nav.Item>
+                                <NavDropdown title="MEDICAL" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#">MBBS</NavDropdown.Item>
                                     <NavDropdown.Item href="#">BDS</NavDropdown.Item>
-                                    <NavDropdown.Item href="#">BAMS</NavDropdown.Item>
-                                    <NavDropdown.Item href="#">BHMS</NavDropdown.Item>
-                                    <NavDropdown.Item href="#">PHARMACY</NavDropdown.Item>
-                                    <NavDropdown.Item href="#">BPT</NavDropdown.Item>
-                                    <NavDropdown.Item href="#">NURSING</NavDropdown.Item>
-                                    <NavDropdown.Item href="#">MBA</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">MDS</NavDropdown.Item>
                                 </NavDropdown>
                             </Nav.Item>
 
+                            <Nav.Item>
+                                <NavDropdown title="AYURVEDA" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#">BAMS</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">BHMS</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">BNYS</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav.Item> */}
+
                             {/* <Nav.Item>
-                                <Link
-                                    activeClass="active"
-                                    to="services"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={800}
-                                    className="nav-link"
-                                    onClick={this.closeNavbar}
-                                >
-                                    Services
-                                </Link>
+                                <NavDropdown title="PHARMACY" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#">B.PHARMA</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">D.PHARMA</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">PHARMD</NavDropdown.Item>
+                                </NavDropdown>
                             </Nav.Item>
 
                             <Nav.Item>
-                                <Link
-                                    activeClass="active"
-                                    to="team"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={800}
-                                    className="nav-link"
-                                    onClick={this.closeNavbar}
-                                >
-                                    Team
-                                </Link>
+                                <NavDropdown title="PHYSIOTHRAPY" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#">BPT</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">GNM</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">BSC.NURSING</NavDropdown.Item>
+                                </NavDropdown>
                             </Nav.Item>
 
                             <Nav.Item>
+                                <NavDropdown title="ENGINEERING" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#">MECHANICAL</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">C S</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">CIVIL</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">ELECTRICAL</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">ECE</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav.Item> */}
+
+                           {/* <Nav.Item>
                                 <Link
                                     activeClass="active"
-                                    to="portfolio"
+                                    to="contact"
                                     spy={true}
                                     smooth={true}
                                     offset={-70}
@@ -130,14 +195,13 @@ class NavBar extends Component {
                                     className="nav-link"
                                     onClick={this.closeNavbar}
                                 >
-                                    Portfolio
+                                    MBA
                                 </Link>
-                            </Nav.Item>
-
-                            <Nav.Item>
+                                </Nav.Item> */}
+                                {/* <Nav.Item>
                                 <Link
                                     activeClass="active"
-                                    to="price"
+                                    to="contact"
                                     spy={true}
                                     smooth={true}
                                     offset={-70}
@@ -145,25 +209,9 @@ class NavBar extends Component {
                                     className="nav-link"
                                     onClick={this.closeNavbar}
                                 >
-                                    Price
+                                    BPT
                                 </Link>
                             </Nav.Item>
-
-                            <Nav.Item>
-                                <Link
-                                    activeClass="active"
-                                    to="blog"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={800}
-                                    className="nav-link"
-                                    onClick={this.closeNavbar}
-                                >
-                                    Blog
-                                </Link>
-                            </Nav.Item>
-
                             <Nav.Item>
                                 <Link
                                     activeClass="active"
@@ -175,7 +223,35 @@ class NavBar extends Component {
                                     className="nav-link"
                                     onClick={this.closeNavbar}
                                 >
-                                    Contact
+                                    BPT
+                                </Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Link
+                                    activeClass="active"
+                                    to="contact"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={800}
+                                    className="nav-link"
+                                    onClick={this.closeNavbar}
+                                >
+                                    NURSING
+                                </Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Link
+                                    activeClass="active"
+                                    to="contact"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={800}
+                                    className="nav-link"
+                                    onClick={this.closeNavbar}
+                                >
+                                    MBA
                                 </Link>
                             </Nav.Item> */}
                                 
@@ -188,6 +264,7 @@ class NavBar extends Component {
                 </Container>
             </Navbar> 
         </React.Fragment> 
+     </DefaultStyle>
     );
   }
 }
